10dbb2f042eefcbd517eb79ae6ec4df638cff717
Chester Enright <amunchet@gmail.com>
2023-02-04 14:09:30 -0600

    chore: setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 # Helper setup script for .env and docker-compose.yml file for autopuller
 # Basically need to make sure the path matches on docker-compose volume
 
+echo "Please enter your GITHUB key (required for API calls)"
+read GITHUBKEY
+
 echo "Please enter github repository (e.g. https://github.com/amunchet/autopuller)"
+read REPONAME
 
 echo "Please enter local path to repository (e.g. /home/amunchet/autopuller).  The docker-compose.yml should be in this folder"
 echo "NOTE: This must be the full path, not a relative path"
+read REPODIR
 
-echo "Please enter docker-compose yml filename (default: docker-compose.yml)"
+read -r -p "Please enter docker-compose yml filename (default: docker-compose.yml)" composefile
+composefile=${composefile:"-docker-compose.yml"}
 
-# MODIFY .env.samle
+read -r -p "Enter watch interval (default is 60)" INTERVAL
+INTERVAL=${INTERVAL:-60}
+
+
+# MODIFY .env.sample
+cp .env.sample .env
+sed -i "s/{{GITHUBKEY}}/$GITHUBKEY/g" .env
+sed -i "s/{{REPONAME}}/${REPONAME//\//\\/}/g" .env
+sed -i "s/{{REPODIR}}/${REPODIR//\//\\/}/g" .env
+sed -i "s/{{INTERVAL}}/$INTERVAL/g" .env
 
 # MODIFY docker-compose.yml.template
+cp docker-compose.yml.template $composefile
+sed -i "s/{{PATH}}/${REPODIR//\//\\/}/g" docker-compose.yml
