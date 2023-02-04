1fbae1fcc261d55441841b27f4c22397d4bf5a74
Chester Enright <amunchet@gmail.com>
2023-02-04 15:56:05 -0600

    fix(test): corrected directory testing

--- tests/test_puller.py
+++ tests/test_puller.py
@@ -44,7 +44,7 @@ def test_restart_service():
     Tests restarting the service
     """
     assert os.getcwd() != "/tmp"
-    assert puller.restart_service(puller.REPODIR, True) == [
+    assert puller.restart_service("/tmp", True) == [
         "docker-compose",
         "up",
         "--build",
